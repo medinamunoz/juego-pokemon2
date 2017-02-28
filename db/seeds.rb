2127ac7b9b2f99906ee10b614c5ca174783a9350
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-02-28 18:10:12 -0300

    starting Pokemon game with scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Pokemon.create(name: "mamoswine" , description:"Mamoswine is an Ice/Ground type Pokémon introduced in Generation 4. It is known as the Twin Tusk Pokémon.")
+Pokemon.create(name:"guszzlord", description:"Guzzlord is a Dark/Dragon type Pokémon introduced in Generation 7. It is known as the Junkivore Pokémon.")
+Pokemon.create(name:"turtonator", description:"Turtonator is a Fire/Dragon type Pokémon introduced in Generation 7. It is known as the Blast Turtle Pokémon.")
+Pokemon.create(name:"tyrunt", description:"Tyrunt is a Rock/Dragon type Pokémon introduced in Generation 6. It is known as the Royal Heir Pokémon.")
+Pokemon.create(name:"pangoro", description:"Pangoro is a Fighting/Dark type Pokémon introduced in Generation 6. It is known as the Daunting Pokémon.")
